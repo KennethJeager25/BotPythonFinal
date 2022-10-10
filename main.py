e5142f904d9951a3f893edd0d9af22270d4e5ce5
KennethJeager25 <95264229+KennethJeager25@users.noreply.github.com>
2022-10-10 18:07:06 -0500

    botfinal

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver import FirefoxOptions
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import FirefoxProfile
 import time
 from classTempCH1 import temp_CH1
 from classTempCH2 import temp_CH2
@@ -23,10 +25,11 @@ class Bot:
         
         self.navegador = './geckodriver'
         self.url='https://mypid.smartpid.com/mypid/'
-        opts = FirefoxOptions()
-        opts.add_argument("--headless")
-
-        driver = webdriver.Firefox(options=opts)
+        options = Options()
+        options.add_argument("-headless")
+        ffprofile = FirefoxProfile()
+        ffprofile.set_preference("intl.accept_languages", "en-US")
+        driver = Firefox(firefox_profile=ffprofile, firefox_options=options)
         driver.get(self.url)
 
         driver.find_element(By.XPATH,'//*[@id="sign-in-container"]/ion-grid/ion-row[1]/ion-col/ion-list/ion-item[1]/ion-input/input').send_keys('marrito@me.com')
